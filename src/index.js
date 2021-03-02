cba4c8d491c63aa0c61f5c5e75892e1329a1ff55
disheg <disheg@gmail.com>
2021-03-02 16:06:40 +0100

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import i18next from 'i18next';
 import init from './init';
+
+i18next.init({
+  lng: 'ru', // Текущий язык
+  debug: true,
+  resources: {
+    ru: { // Тексты конкретного языка
+      translation: { // Так называемый namespace по умолчанию
+        key: 'Привет мир!',
+        urlRequired: 'Необходимо заполнить URL',
+        wrongUrl: 'Неправильный URL',
+        hasAlready: 'RSS уже существует',
+        rssLoaded: 'RSS успешно загружен',
+      },
+    },
+  },
+});
 
 init();
