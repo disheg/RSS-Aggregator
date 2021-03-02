c83d8d88b2ef6d83e1a0b14ba22b31ad81961816
disheg <disheg@gmail.com>
2021-03-02 16:13:36 +0100

    refactoring

--- src/app.js
+++ src/app.js
@@ -80,6 +80,22 @@ const render = (storage) => {
 };
 
 export default () => {
+  i18next
+    .init({
+      lng: 'ru', // Текущий язык
+      debug: true,
+      resources: {
+        ru: { // Тексты конкретного языка
+          translation: { // Так называемый namespace по умолчанию
+            key: 'Привет мир!',
+            urlRequired: 'Необходимо заполнить URL',
+            wrongUrl: 'Неправильный URL',
+            hasAlready: 'RSS уже существует',
+            rssLoaded: 'RSS успешно загружен',
+          },
+        },
+      },
+    });
   console.log('It work');
   const form = document.querySelector('#form-rss');
   const input = form.elements.host;
--- src/init.js
+++ src/init.js
@@ -1,22 +1,5 @@
-import i18next from 'i18next';
 import app from './app';
 
 export default () => {
-  i18next
-    .init({
-      lng: 'ru', // Текущий язык
-      debug: true,
-      resources: {
-        ru: { // Тексты конкретного языка
-          translation: { // Так называемый namespace по умолчанию
-            key: 'Привет мир!',
-            urlRequired: 'Необходимо заполнить URL',
-            wrongUrl: 'Неправильный URL',
-            hasAlready: 'RSS уже существует',
-            rssLoaded: 'RSS успешно загружен',
-          },
-        },
-      },
-    })
-    .then(() => app());
+  app();
 };
