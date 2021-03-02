7a114d52f2a487887e11bf5387ca2ceebf38dc5b
disheg <disheg@gmail.com>
2021-03-02 13:47:02 +0100

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   module: {
     rules: [
       {
