f539ad2650b530f5115eae6d7ff4d9f30198f29a
disheg <disheg@gmail.com>
2021-03-02 16:09:40 +0100

    refactoring

--- src/init.js
+++ src/init.js
@@ -1,5 +1,22 @@
+import i18next from 'i18next';
 import app from './app';
 
 export default () => {
-  app();
+  i18next
+    .init({
+      lng: 'ru', // Текущий язык
+      debug: true,
+      resources: {
+        ru: { // Тексты конкретного языка
+          translation: { // Так называемый namespace по умолчанию
+            key: 'Привет мир!',
+            urlRequired: 'Необходимо заполнить URL',
+            wrongUrl: 'Неправильный URL',
+            hasAlready: 'RSS уже существует',
+            rssLoaded: 'RSS успешно загружен',
+          },
+        },
+      },
+    })
+    .then(() => app());
 };
