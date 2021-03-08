b56ba1b44c9404ab035cc2364279e83d6d0e8808
disheg <disheg@gmail.com>
2021-03-08 12:36:37 +0100

    refactoring

--- src/app.js
+++ src/app.js
@@ -55,7 +55,14 @@ const getFeedAndPosts = (data) => {
 };
 
 const render = (storage, elements, i18next) => {
-  const { feeds, posts } = elements;
+  const {
+    feeds,
+    posts,
+    titleModal,
+    descriptionModal,
+    link,
+  } = elements;
+
   const renderFeeds = (data) => data.map(({ title, description }) => (`
       <li class="list-group-item">
         <h3>${title}</h3>
@@ -63,7 +70,7 @@ const render = (storage, elements, i18next) => {
       </li>`));
   const renderPosts = (data) => data.map(({ title, id, link }) => (`
     <li class="list-group-item d-flex justify-content-between align-items-start">
-      <a href="${link}" class="font-weight-bold" target="_blank" rel="noopener noreferrer">
+      <a href="${link}" data-id="${id}" class="font-weight-bold" target="_blank" rel="noopener noreferrer">
         ${title}
       </a>
       <button id="btn-modal" type="button" class="btn btn-primary" data-id="${id}" data-toggle="modal" data-target="#rssModal">
@@ -80,16 +87,20 @@ const render = (storage, elements, i18next) => {
       ${renderPosts(storage.posts).join('')}
     </ul>`;
 
+  
   const btns = document.querySelectorAll('#btn-modal');
   btns.forEach((btn) => btn.addEventListener('click', (e) => {
     const { id } = e.target.dataset;
+    const a = document.querySelector(`a[data-id="${id}"]`);
+    a.classList.remove('font-weight-bold');
     const data = storage.posts.find((post) => post.id === id);
     const { title, description, url } = data;
-    console.log(data)
-    elements.titleModal.textContent = title;
-    elements.descriptionModal.textContent = description;
-    elements.link.href = url;
-  }))
+    console.log('data', data)
+    titleModal.textContent = title;
+    descriptionModal.textContent = description;
+    link.href = url;
+    console.log('title', titleModal.textContent)
+  }));
 };
 
 export default () => {
@@ -161,7 +172,7 @@ export default () => {
         submitButton.disabled = true;
         break;
       case 'failed':
-        input.toggleAttribute('readonly');
+        input.removeAttribute('readonly');
         submitButton.disabled = false;
         handleError();
         state.formProcess.state = '';
